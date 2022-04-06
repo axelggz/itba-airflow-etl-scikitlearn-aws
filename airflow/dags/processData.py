55ab3c7c6f2103dcf48149059e896549c9bb2f89
axelggz <axel.garcia.gz@gmail.com>
2022-04-06 15:17:18 -0300

    Test commit

--- airflow/dags/processData.py
+++ airflow/dags/processData.py
@@ -8,10 +8,10 @@ from airflow.models import dag
 from airflow.utils.dates import days_ago
 from airflow.operators.python_operator import PythonOperator
 
-#import BLL.DoCalcs as DoCalcs
+import BLL.DoCalcs as DoCalcs
 
 args={
-    'owner': 'nacho',
+    'owner': 'axel',
 }
 
 dt = datetime.strptime('19 Aug 2021', '%d %b %Y')
@@ -26,9 +26,7 @@ dag = dag.DAG(
     catchup=False)
 
 def _proceso(**kwargs):
-    #DoCalcs.calculateDelay(kwargs['year'])
-    print('a')
-    # print(numpy.__file__)
+    DoCalcs.calculateDelay(kwargs['year'])
 
 with dag:
 
@@ -38,3 +36,68 @@ with dag:
         provide_context=True,
         op_kwargs={'year': '2009'}
     )
+    
+    procesar_2010 = PythonOperator(
+        task_id = 'procesar_2010',
+        python_callable=_proceso,
+        provide_context=True,
+        op_kwargs={'year': '2010'}
+    )
+
+    procesar_2011 = PythonOperator(
+        task_id = 'procesar_2011',
+        python_callable=_proceso,
+        provide_context=True,
+        op_kwargs={'year': '2011'}
+    )
+
+    procesar_2012 = PythonOperator(
+        task_id = 'procesar_2012',
+        python_callable=_proceso,
+        provide_context=True,
+        op_kwargs={'year': '2012'}
+    )
+
+    procesar_2013 = PythonOperator(
+        task_id = 'procesar_2013',
+        python_callable=_proceso,
+        provide_context=True,
+        op_kwargs={'year': '2013'}
+    )
+
+    procesar_2014 = PythonOperator(
+        task_id = 'procesar_2014',
+        python_callable=_proceso,
+        provide_context=True,
+        op_kwargs={'year': '2014'}
+    )
+
+    procesar_2015 = PythonOperator(
+        task_id = 'procesar_2015',
+        python_callable=_proceso,
+        provide_context=True,
+        op_kwargs={'year': '2015'}
+    )
+
+    procesar_2016 = PythonOperator(
+        task_id = 'procesar_2016',
+        python_callable=_proceso,
+        provide_context=True,
+        op_kwargs={'year': '2016'}
+    )
+
+    procesar_2017 = PythonOperator(
+        task_id = 'procesar_2017',
+        python_callable=_proceso,
+        provide_context=True,
+        op_kwargs={'year': '2017'}
+    )
+
+    procesar_2018 = PythonOperator(
+        task_id = 'procesar_2018',
+        python_callable=_proceso,
+        provide_context=True,
+        op_kwargs={'year': '2018'}
+    )
+
+    procesar_2009 >> procesar_2010 >> procesar_2011 >> procesar_2012 >> procesar_2013 >> procesar_2014 >> procesar_2015 >> procesar_2016 >> procesar_2017 >> procesar_2018
